43471e041a4da9a106a86767dc2cdd968505718c
tsumagariDX <tsumagari11dx@gmail.com>
2026-04-01 12:54:50 +0900

    Update sales_report.py

--- sales_report.py
+++ sales_report.py
@@ -60,7 +60,7 @@ def calc_category_sales(records):
     
     return category_sales
 
-def show_report(item_sales, category_sales, skip_count):
+def show_report(item_sales, category_sales, top_item, top_category, skip_count):
     print("商品別売上: ")
     print()
     for item in item_sales:
@@ -71,11 +71,17 @@ def show_report(item_sales, category_sales, skip_count):
     for category in category_sales:
         print(f"{category} : {category_sales[category]}")
 
+    print("売上トップ商品: ")
+    print(top_item)
+
+    print("売上トップカテゴリ: ")
+    print(top_category)
+
     print("スキップ件数: ")
     print()
     print(f"{skip_count} 件")
 
-def save_report(output_path, item_sales, category_sales, skip_count):
+def save_report(output_path, item_sales, category_sales, top_item, top_category, skip_count):
     with open(output_path, "w", encoding="utf-8") as f:
         f.write("商品別売上: \n")
         for item in item_sales:
@@ -84,17 +90,38 @@ def save_report(output_path, item_sales, category_sales, skip_count):
         for category in category_sales:
             f.write(f"{category} : {category_sales[category]}\n")
 
+        f.write("売上トップ商品: \n")
+        f.write(f"{top_item}\n")
+
+        f.write("売上トップカテゴリ: \n")
+        f.write(f"{top_category}\n")
+
         f.write("スキップ件数: \n")
         f.write(f"{skip_count} 件\n")
 
 def get_top_item(item_sales):
     max_price = 0
-    max_price_item = []
+    max_price_item = ""
     for item in item_sales:
-        if item[item] > max_price:
-            max_price = item[item]
-            max_price_item = item["item"]
+        if item_sales[item] > max_price:
+            max_price = item_sales[item]
+            max_price_item = item
     return  max_price_item
+
+def sort_top_item(item_sales):
+    sorted_top_item= {}
+    for item in item_sales:
+        sorted_top_item = sorted(item_sales.item(), key=lambda x: x[1], reverse=True)
+    return sorted_top_item
+
+def get_top_category(category_sales):
+    max_sales = 0
+    max_sales_category = ""
+    for category in category_sales:
+        if category_sales[category] > max_sales:
+            max_sales = category_sales[category]
+            max_sales_category = category
+    return max_sales_category
 
 def main():
     input_path = "sales.csv"
@@ -103,8 +130,10 @@ def main():
     records, skip_count = read_sales_csv(input_path)
     item_sales = calc_item_sales(records)
     category_sales = calc_category_sales(records)
-    show_report(item_sales, category_sales, skip_count)
-    save_report(output_path, item_sales, category_sales, skip_count)
+    top_item = get_top_item(item_sales)
+    top_category = get_top_category(category_sales)
+    show_report(item_sales, category_sales, top_item, top_category, skip_count)
+    save_report(output_path, item_sales, category_sales, top_item, top_category, skip_count)
 
 if __name__ == "__main__":
     main()
